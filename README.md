45cfcd9921a367a4eff798387d22b5b1e8917c98
Matt Eagar <matthew_eagar@intuit.com>
2013-12-17 17:21:14 -0500

    Initial revision

---
+++ README.md
@@ -0,0 +1,257 @@
+What is it?
+===========
+`fieldChooser.js` is a [jQuery][] plugin for selecting from a list of fields or values (the source) and adding them to a new list (the destination). You move fields from the source to the destination by dragging and dropping. Fields themselves can be simple HTML elements or more complex containers. 
+
+Why would I use it?
+===================
+`fieldChooser` builds off the jQuery UI [Sortable][] interaction, which allows for dragging and dropping of an HTML element between two containers. `fieldChooser` creates and manages the interaction between the containers, and adds the following capabilities:
+
+- **Multi-select capability.** Shift-click for contiguous selections, control-click (Command-click on Mac) for discontiguous selections. Once selected, multiple items may be dragged and dropped.
+- **Keyboard support.** Navigate through a list using the up and down arrow keys. Hold down the shift key while pressing up or down to select multiple items.
+
+
+Dependencies
+============
+`progressSpin.js` uses the [jQuery][] library for DOM manipulation, and [jQuery UI][] for drag and drop support. 
+
+How do I use it?
+================
+
+1. Create a `<div>` to hold the field chooser. 
+2. Create a [jQuery object][] for the `<div>`, and invoke the `fieldChooser()` method on it to instantiate and return the field chooser.
+3. Put content into source or destination lists using the `add()` method on the list.
+
+For example:  
+```javascript  
+var $chooser = $("#fieldChooser").fieldChooser();
+var $sourceFields = $("#sourceFields").children();
+$chooser.getSourceList().add($sourceFields);
+
+```
+
+Function reference
+==================
+
+`fieldChooser` Methods
+----------------------
+The `fieldChooser` houses two containers, one for the source elements (fields), and another for the destination elements. Each of these two containers is a `fieldList` object.  
+
+NOTE: In order to enable keyboard support, all clickable elements (field lists, fields) within a `fieldChooser` are assigned a `tabIndex` attribute. They will inherit this value from the parent element on which the `fieldChooser()` method is invoked, but if this element does not have one, they will assume a default `tabIndex` value of 0.
+
+###fieldChooser(options)###
+Creates a new field chooser and binds it to the `<div>` on which this method is called. If invoked more than once on the same [jQuery object][], will return a single instance; however, if you create a new [jQuery object][] for the same div, you will end up with more than one chooser.
+
+**parameters**  
+*options* (object): Configuration options for the field chooser instance. Currently, there are no configuration options, so this is just a placeholder for future functionality.
+
+**returns**  
+The field chooser instance.
+
+###getOptions()###
+Retrieves the configuration options for the field chooser instance. 
+
+**parameters**  
+(none)
+
+**returns**  
+An object containing the configuration options for the instance.
+
+###getSourceList()###
+Retrieves the field list containing all of the source fields. (The source is the left-hand container in the field chooser.)
+
+**parameters**  
+(none)
+
+**returns**  
+A `fieldList` object containing the source fields.
+
+###getDestinationList()###
+Retrieves the field list containing all of the destination fields. (The destination is the right-hand container in the field chooser.)
+
+**parameters**  
+(none)
+
+**returns**  
+A `fieldList` object containing the destination fields.
+
+###getFieldList(field)###
+Gets the container for a particular field.
+
+**parameters**  
+*field* ([jQuery object][]): A field (or set of fields) in either the source or destination list within the chooser.
+
+**returns**  
+The `fieldList` object containing the field. 
+
+`fieldChooser` Events
+---------------------
+###listChanged###
+Fired when fields move from one list to another. 
+
+**callback parameters**  
+*event* ([jQuery event][]): The jQuery event invoking the callback.
+
+*selection* ([jQuery object][]): The field (or set of fields) which has moved.
+
+*list* (`fieldList`): The field list to which the selection has moved.
+
+`fieldList` Methods
+-------------------
+A `fieldList` is a jQuery [Sortable][] container (a `<div>`) which holds the "fields" in the chooser. Fields can be whatever you need - any HTML element will work, so they can be as simple or complex as necessary. 
+
+Because the `fieldList` is a [Sortable][] object, you can set any of the [Sortable][] options, call [Sortable][] methods, or handle [Sortable][] events. However, you should avoid overriding the following options, or you may interfere with `fieldChooser` functionality:
+
+- `connectWith`: This is already set by the parent `fieldChooser`, so modifying this value will break the drag/drop operation of the chooser.  
+- `helper`: The `fieldChooser` uses a custom helper to manage the movement of multiple items at once. 
+
+###add(content)###
+Adds content (fields) to the list.
+
+**parameters**  
+*content* ([jQuery object][]): A field (or set of fields) to add to the list.
+
+**returns**  
+(none)
+
+###getFields()###
+Retrieves all of the fields in the list.
+
+**parameters**  
+(none)
+
+**returns**  
+A [jQuery object][] containing all of the fields in the list.
+
+###getSelection()###
+Retrieves the selected fields in the list.
+
+**parameters**  
+(none)
+
+**returns**  
+A [jQuery object][] containing all of the selected fields in the list.
+
+###getSelectedIndex()###
+Retrieves the index of the current selection. This is a single value pointing to the last individually selected item in the list. 
+
+**parameters**  
+(none)
+
+**returns**  
+The index value (an integer) of the last individually selected item in the list. If there is no selection, returns -1. 
+
+###getExtendedSelectionIndex()###
+Retrieves the index of the last item to which the selection was extended. You can extend a selection by using the shift key, or invoking the `selectTo()` or `extendSelection()` method.
+
+**parameters**  
+(none)
+
+**returns**  
+The index value (an integer) of the last item in an extended selection. If there is no selection, or if the selection has not been extended, returns -1. 
+
+###clearSelection()###
+Clears the set of selected fields in the list.
+
+**parameters**  
+(none)
+
+**returns**  
+(none)
+
+###selectField(field)###
+Selects a single field in the list. If other fields are selected, they will be deselected after this operation. This is the programmatic equivalent of clicking on a field.
+
+**parameters**  
+*field* ([jQuery object][]): The field to select.
+
+**returns**  
+(none)
+
+###selectAt(index)###
+Selects the field at a given index. If other fields are selected, they will be deselected after this operation. This is the programmatic equivalent of clicking on a field. 
+
+**parameters**  
+*index* (integer): The index of the field to select. 
+
+**returns**  
+(none)
+
+###toggleFieldSelection(field)###
+Toggles a field between selected or deselected states. Retains any previous field selections. This is the programmatic equivalent of control-clicking on a field.
+
+**parameters**  
+*field* ([jQuery object][]): The field to toggle. 
+
+**returns**  
+(none)
+
+###selectTo(fieldOrIndex)###
+Extends the current selection to the field or field index specified. If there is no existing selection, starts a new one beginning with the first field.
+
+**parameters**  
+*fieldOrIndex* ([jQuery object][] or integer): The field or the index of the field to which the selection should be extended.
+
+**returns**  
+(none)
+
+###extendSelection(up)###
+Extends the current selection by a single field in the direction specified. If there is no existing selection, begins a new one at the far end of the list - i.e., if the direction is up, starts at the bottom; if the direction is down, starts at the top.
+
+**parameters**  
+*up* (boolean): Indicates the direction in which the selection should be extended. Pass `true` to extend the selection "up" the list (i.e., decreasing index values); `false` to extend down.
+
+**returns**  
+(none)
+
+`fieldList` Events
+------------------
+###selectionChanged###
+Fired when the selection changes on a list. 
+
+**callback parameters**  
+*list* (`fieldList`): The list on which the selection changed.
+
+Styling reference
+==================
+`fieldChooser` uses classes to identify subcomponents, so you can freely style the entire control using CSS. It defines the following classes:
+
+###fc-field-list###
+Applied to all `fieldList` objects (source and destination) in the control. 
+
+**Recommended properties**  
+Some kid of border
+overflow: scroll
+
+###fc-source-fields###
+Applied to the "source" field list. 
+
+**Recommended properties**  
+float: left
+
+###fc-destination-fields###
+Applied to the "destination" field list.
+
+**Recommended properties**  
+float: right
+
+###fc-field###
+Applied to fields in a `fieldList`.
+
+###fc-selected###
+Applied to selected fields in a `fieldList`.
+
+
+License
+=======
+`fieldChooser.js` is free to use under the MIT license, which is included at the top of the un-minified version of the source. If you use the un-minified version, please include the license text as provided. 
+
+I wrote this plugin while working for [Intuit][]'s [QuickBase][] team; Intuit is the copyright holder. 
+
+
+[jQuery]: http://jquery.com/ 
+[jQuery UI]: http://jqueryui.com/
+[Sortable]: http://jqueryui.com/sortable/
+[jQuery object]: http://api.jquery.com/Types/#jQuery
+[jQuery event]: http://api.jquery.com/Types/#Event
+[Intuit]: http://www.intuit.com/ 
+[QuickBase]: http://quickbase.intuit.com/ 
---
+++ fieldChooser.js
@@ -0,0 +1,448 @@
+// Copyright (c) 2013 Intuit, Inc
+// Author: Matthew Eagar (matthew_eagar@intuit.com)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+(function($) {
+    $.fn.fieldChooser = function (options) {
+        //----------------------------------------------------------------------
+        // Poor man's singleton 
+        //----------------------------------------------------------------------
+        if (this.getOptions) {
+            return this;
+        }
+        
+        //----------------------------------------------------------------------
+        // Private methods 
+        //----------------------------------------------------------------------
+        function getBounds(element) {
+            var offset = element.offset();
+            return {
+                left: offset.left,
+                right: offset.left + element.width(),
+                top: offset.top,
+                bottom: offset.top + element.height()
+            }
+        }
+        
+        function translateBounds(bounds, newPosition) {
+            return {
+                left: newPosition.left,
+                right: bounds.right + newPosition.left - bounds.left,
+                top: newPosition.top,
+                bottom: bounds.bottom + newPosition.top - bounds.top
+            }
+        }
+        
+        function hitTest(container, element, newPosition) {
+            var containerBounds = getBounds(container);
+            var elementBounds = getBounds(element);
+            elementBounds = translateBounds(elementBounds, newPosition);
+
+            var hit = true;
+            if (elementBounds.right < containerBounds.left) {
+                hit = false;
+            }
+            else if (elementBounds.left > containerBounds.right) {
+                hit = false;
+            }
+            else if (elementBounds.bottom < containerBounds.top) {
+                hit = false;
+            }
+            else if (elementBounds.top > containerBounds.bottom) {
+                hit = false;
+            }
+            return hit;
+        }
+        
+        var _chooser = this;
+        var _lastSelectionList = null;
+        function onListSelectionChanged(event, list) {
+            if (_lastSelectionList) {
+                if (_lastSelectionList == list) {
+                    // continue
+                }
+                else {
+                    var otherList = _chooser.getSourceList();
+                    if (list == _chooser.getSourceList()) {
+                        otherList = _chooser.getDestinationList();
+                    }
+                    otherList.clearSelection();
+                }
+            }
+            _lastSelectionList = list;
+        }
+
+        //----------------------------------------------------------------------
+        // fieldList class 
+        //----------------------------------------------------------------------
+        var fieldList = function (parent, tabIndex) {
+            var _list = $("<div class='fc-field-list' tabIndex='" + tabIndex + 
+                          "'></div>");
+            _list.appendTo(parent);
+            
+            var _selectedIndex = -1;
+            var _extendedSelectionIndex = -1;
+            
+            _list.selectAt = function (index) {
+                this.clearSelection();
+                var fields = _list.getFields();
+                if (index >= fields.length) {
+                    index = fields.length - 1;
+                }
+                
+                var selectedField = null;
+                if (index >= 0) {
+                    selectedField = fields.eq(index);
+                }
+
+                if (selectedField) {
+                    selectedField.addClass("fc-selected");
+                    _selectedIndex = index;
+                }
+                else {
+                    _selectedIndex = -1;
+                }
+                _list.trigger("selectionChanged", [_list]);
+            }
+            
+            _list.extendSelection = function (up) {
+                var selectedIndex = this.getSelectedIndex();
+                var extendedIndex = this.getExtendedSelectionIndex();
+                var newIndex = extendedIndex;
+                var extend = true;
+                if (up) {
+                    if (extendedIndex > selectedIndex) {
+                        extend = false;
+                    }
+                    else {
+                        newIndex = extendedIndex - 1;
+                    }
+                }
+                else {
+                    if (extendedIndex < selectedIndex) {
+                        extend = false;
+                    }
+                    else {
+                        newIndex = extendedIndex + 1;
+                    }
+                }
+                
+                var fields = _list.getFields();
+                if (newIndex < 0 || newIndex >= fields.length) {
+                    // continue
+                }
+                else {
+                    var selectedField = fields.eq(newIndex);
+                    if (extend) {
+                        selectedField.addClass("fc-selected");
+                    }
+                    else {
+                        selectedField.removeClass("fc-selected");
+                        if (up) {
+                            newIndex--;
+                        }
+                        else {
+                            newIndex++;
+                        }
+                    }
+                    _list.trigger("selectionChanged", [_list]);
+                    _extendedSelectionIndex = newIndex;
+                }
+            }
+            
+            _list.selectField = function (field) {
+                this.clearSelection();
+                field.addClass("fc-selected");
+                _selectedIndex = field.index();
+                _list.trigger("selectionChanged", [_list]);
+            }
+            
+            _list.toggleFieldSelection = function (field) {
+                field.toggleClass("fc-selected");
+                if (field.hasClass("fc-selected")) {
+                    _selectedIndex = field.index();
+                    _extendedSelectionIndex = -1;
+                }
+                else {
+                    if (_selectedIndex == field.index()) {
+                        _selectedIndex = _list.children(".fc-selected").first().index();
+                    }
+                }
+                _list.trigger("selectionChanged", [_list]);
+            }
+            
+            _list.selectTo = function (fieldOrIndex) {
+                var fieldIndex = fieldOrIndex;
+                if (typeof fieldOrIndex == "object") {
+                    fieldIndex = fieldOrIndex.index();
+                }
+                
+                if (_selectedIndex == -1) {
+                    _selectedIndex = 0;
+                }
+                
+                var children = _list.children();
+                if (fieldIndex > _selectedIndex) {
+                    for (var counter = _selectedIndex; 
+                         counter < children.length; 
+                         counter++) {
+                        if (counter <= fieldIndex) {
+                            children.eq(counter).addClass("fc-selected");
+                        }
+                        else {
+                            children.eq(counter).removeClass("fc-selected");
+                        }
+                    }                    
+                }
+                else {
+                    for (var counter = _selectedIndex; 
+                         counter >= 0; 
+                         counter--) {
+                        if (counter >= fieldIndex) {
+                            children.eq(counter).addClass("fc-selected");
+                        }
+                        else {
+                            children.eq(counter).removeClass("fc_selected");
+                        }
+                    }
+                }
+                
+                _extendedSelectionIndex = fieldIndex;
+                
+                _list.trigger("selectionChanged", [_list]);
+            }
+            
+            _list.getSelectedIndex = function () {
+                return _selectedIndex;
+            }
+            
+            _list.getExtendedSelectionIndex = function () {
+                var index = _extendedSelectionIndex;
+                if (index < 0) {
+                    index = _selectedIndex;
+                }
+                return index;
+            }
+            
+            _list.getSelection = function () {
+                return this.children(".fc-selected");
+            }
+            
+            _list.clearSelection = function () {
+                _selectedIndex = -1;
+                _extendedSelectionIndex = -1;
+                this.children().removeClass("fc-selected");
+            }
+            
+            _list.add = function(content) {
+                content.addClass("fc-field");
+                content.attr("tabIndex", tabIndex);
+                content.on("click", function(event) {
+                    var $this = $(this);
+                    if (event.ctrlKey || event.metaKey) {
+                        _chooser.getFieldList($this).toggleFieldSelection($this);
+                    }
+                    else if (event.shiftKey) {
+                        _chooser.getFieldList($this).selectTo($this);
+                    }
+                    else {
+                        _chooser.getFieldList($this).selectField($this);
+                    }
+                    _currentList = _list;
+                });
+                content.appendTo(_list);
+
+                return _list;
+            };
+            
+            _list.getFields = function () {
+                return _list.children();
+            }
+            
+            _list.sortable({
+                connectWith: ".fc-field-list",
+                cursor: "move",
+                opacity: 0.75,
+                helper: function (event, field) {
+                    if (field.hasClass("fc-selected")) {
+                        // continue
+                    }
+                    else {
+                        _list.selectField(field);
+                    }
+                    
+                    var selection = _list.getSelection().clone();
+                    field.data("selection", selection);
+                    field.siblings(".fc-selected").remove();
+                    var helper = $("<div/>").append(selection);
+                    
+                    return helper;
+                }
+            });
+            
+            return _list;
+        };
+    
+        //----------------------------------------------------------------------
+        // Private variables 
+        //----------------------------------------------------------------------
+        var _options = $.extend({}, $.fn.fieldChooser.defaults, options);
+        var tabIndex = parseInt(this.attr("tabIndex"));
+        if (tabIndex == NaN) {
+            tabIndex = 0;
+        }
+        this.removeAttr("tabIndex");
+        var _sourceList = new fieldList(this, tabIndex).addClass("fc-source-fields");
+        var _destinationList = 
+            new fieldList(this, tabIndex).addClass("fc-destination-fields");
+        var _currentList = null;
+
+        //----------------------------------------------------------------------
+        // Public properties 
+        //----------------------------------------------------------------------
+        this.getOptions = function () {
+            return _options;
+        };
+        
+        this.getSourceList = function () {
+            return _sourceList;
+        };
+        
+        this.getDestinationList = function () {
+            return _destinationList;
+        };
+        
+        this.getFieldList = function (field) {
+            var list = _destinationList;
+            if (field.parent().hasClass("fc-source-fields")) {
+                list = _sourceList;
+            }
+            return list;
+        }
+        
+        //----------------------------------------------------------------------
+        // Public methods
+        //----------------------------------------------------------------------
+
+        //----------------------------------------------------------------------
+        // Event handlers 
+        //----------------------------------------------------------------------
+        _destinationList.on("sortstop", function (event, ui) {
+            var selection = ui.item.data("selection");
+            
+            if (hitTest(_sourceList, ui.item, ui.offset)) {
+                _sourceList.add(selection);
+                _chooser.trigger("listChanged", [selection, _sourceList]);
+                ui.item.after(selection).remove();
+            }
+            else if (hitTest(_destinationList, ui.item, ui.offset)) {
+                ui.item.after(selection).remove();
+            }
+            else {
+                _destinationList.getSelection().remove();
+                _sourceList.add(selection);
+                _lastSelectionList = _sourceList;
+                _chooser.trigger("listChanged", [selection, _sourceList]);
+            }
+        });
+        
+        _sourceList.on("sortstop", function (event, ui) {
+            var selection = ui.item.data("selection");
+
+            if (hitTest(_destinationList, ui.item, ui.offset)) {
+                _destinationList.add(selection);
+                _chooser.trigger("listChanged", [selection, _destinationList]);
+            }
+            else if (hitTest(_sourceList, ui.item, ui.offset)) {
+                // continue
+            }
+            else {
+                _sourceList.sortable("cancel");
+            }
+
+            ui.item.after(selection).remove();
+        });
+        
+        _destinationList.on("selectionChanged", onListSelectionChanged);
+        _sourceList.on("selectionChanged", onListSelectionChanged);
+        
+        _destinationList.on("focusin", function () {
+            _currentList = _destinationList;
+        });
+        
+        _destinationList.on("focusout", function () {
+            if (_currentList == _destinationList) {
+                _currentList = null;
+            }
+        });
+        
+        _sourceList.on("focusin", function () {
+            _currentList = _sourceList;
+        });
+        
+        _sourceList.on("focusout", function () {
+            if (_currentList == _sourceList) {
+                _currentList = null;
+            }
+        });
+        
+        $(document).keydown(function () {
+            if (_currentList) {
+                if (event.which == 38) { // up arrow
+                    if (event.shiftKey) {
+                        _currentList.extendSelection(true);
+                    }
+                    else {
+                        var selectedIndex = _currentList.getSelectedIndex();
+                        var newIndex = selectedIndex - 1;
+                        if (newIndex < 0) {
+                            newIndex = 0;
+                        }
+                        _currentList.selectAt(newIndex);
+                    }
+                }
+                else if (event.which == 40) { // down arrow 
+                    if (event.shiftKey) {
+                        _currentList.extendSelection(false);
+                    }
+                    else {
+                        var selectedIndex = _currentList.getSelectedIndex();
+                        var newIndex = selectedIndex + 1;
+                        if (selectedIndex < 0) {
+                            newIndex = _currentList.getFields().length - 1;
+                        }
+                        _currentList.selectAt(newIndex);
+                    }
+                }
+                else if (event.which == 27) { // escape
+                    _currentList.selectAt(-1);
+                }
+            }
+        });
+            
+        return this;
+    };
+    
+    //--------------------------------------------------------------------------
+    // Class defaults
+    //--------------------------------------------------------------------------
+    $.fn.fieldChooser.defaults = {};
+    
+}(jQuery));
