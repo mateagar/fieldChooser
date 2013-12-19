18af63e969bb64a71f5ba7bb4117879dda6070af
Matt Eagar <matthew_eagar@intuit.com>
2013-12-19 14:21:13 -0500

    Fixed a couple of issues with extendSelection()
    1. When nothing is selected in a list and shift + up arrow pressed,
    nothing was being selected. Now begins selecting from the bottom of the
    list.
    2. When nothing is selected in a list and shift + either arrow pressed,
    now sets the selected index for the list.

--- fieldChooser.js
+++ fieldChooser.js
@@ -1,24 +1,3 @@
-// Copyright (c) 2013 Intuit, Inc
-// Author: Matthew Eagar (matthew_eagar@intuit.com)
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 (function($) {
     $.fn.fieldChooser = function (options) {
         //----------------------------------------------------------------------
@@ -128,19 +107,28 @@
                 var newIndex = extendedIndex;
                 var extend = true;
                 if (up) {
+                    if (newIndex < 0) {
+                        newIndex = _list.getFields().length;
+                        _selectedIndex = newIndex - 1;
+                    }
+
                     if (extendedIndex > selectedIndex) {
                         extend = false;
                     }
                     else {
-                        newIndex = extendedIndex - 1;
+                        newIndex--;
                     }
                 }
                 else {
+                    if (newIndex < 0) {
+                        _selectedIndex = 0;
+                    }
+                    
                     if (extendedIndex < selectedIndex) {
                         extend = false;
                     }
                     else {
-                        newIndex = extendedIndex + 1;
+                        newIndex++;
                     }
                 }
                 
